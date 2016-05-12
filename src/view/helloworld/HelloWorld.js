1efe52c7fd30f13e231f64f54c284162361d05bf
Mohamed Labib <lebaz20@gmail.com>
2016-05-12 16:03:48 +0200

    Restructure helloworld example to fit as view component

--- src/view/helloworld/HelloWorld.js
+++ src/view/helloworld/HelloWorld.js
@@ -5,14 +5,17 @@ export class HelloWorld extends React.Component {
   render () {
     return (
       <p>
-        Hello thereererererer, <input type="text" placeholder="Your name here" defaultValue="asd" />!
+        Hello world,
         It is {this.props.date.toTimeString()}
       </p>
     );
   }
 }
 
-render(
-  <HelloWorld date={new Date()} />,
-  document.getElementById('example')
-);
+var exampleElement = document.getElementById('example');
+if(exampleElement){
+  render(
+    <HelloWorld date={new Date()} />,
+    exampleElement
+  );
+}
