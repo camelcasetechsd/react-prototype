e50e253ce1004ea196d7317986ab7916a60f1561
Mohamed Labib <lebaz20@gmail.com>
2016-05-15 16:10:42 +0200

    Update scss styles to be integrated inside app.js which is handled by webpack

--- src/view/app.js
+++ src/view/app.js
@@ -2,3 +2,6 @@ import {HelloWorld} from './helloworld/js/HelloWorld.js';
 
 import {CommentStructure} from './comments/js/CommentStructure.js';
 import {CommentBox} from './comments/js/CommentBox.js';
+
+// require styles to be recognized by webpack to compile them into separate files
+require("./app.scss");
