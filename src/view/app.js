e7e0809b96716166a07f9c4d667da6adc87f14a1
Mohamed Labib <lebaz20@gmail.com>
2016-05-16 19:47:18 +0200

    Add crud html and integrate view js and style

--- src/view/app.js
+++ src/view/app.js
@@ -3,5 +3,7 @@ import {HelloWorld} from './modules/helloworld/js/HelloWorld.js';
 import {CommentStructure} from './modules/comments/js/CommentStructure.js';
 import {CommentBox} from './modules/comments/js/CommentBox.js';
 
+import {NoteBox} from './modules/crud/js/NoteBox.js';
+
 // require styles to be recognized by webpack to compile them into separate files
 require("./app.scss");
